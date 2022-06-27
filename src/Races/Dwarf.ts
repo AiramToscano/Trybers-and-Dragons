459c53ea4ca452eb93da1eba444021f5e729094f
Airam Toscano <airamlobato@gmail.com>
2022-06-27 16:51:17 -0400

    Feat: Dwarf.ts

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,19 @@
+import Race from './Race';
+
+export default class Dwarf extends Race {
+  private _maxLifePoints: number;
+  static numinstacia = 0;
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 80;
+    Dwarf.numinstacia += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances():number {
+    return Dwarf.numinstacia;
+  }
+}
