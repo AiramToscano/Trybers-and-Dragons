5568649af5e1ab585ce7513611942b1a21ccc6bb
Airam Toscano <airamlobato@gmail.com>
2022-06-27 18:21:52 -0400

    Feat: Ranger.ts

---
+++ src/Archetypes/Ranger.ts
@@ -0,0 +1,20 @@
+import Archetype from './Archetype';
+import { EnergyType } from '../Energy';
+
+export default class Ranger extends Archetype {
+  private _energyType: EnergyType;
+  static numinstacia = 0;
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'stamina';
+    Ranger.numinstacia += 1;
+  }
+
+  get energyType(): EnergyType { 
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances():number {
+    return Ranger.numinstacia;
+  }
+}
